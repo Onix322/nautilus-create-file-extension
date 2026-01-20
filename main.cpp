8be0b2d9055e08c17dc22f70e85aa4c63ec5a521
Onix322 <alexandru.dobos3@gmail.com>
2026-01-20 10:33:36 +0100

    Added a feature to use the extension in root directories

--- main.cpp
+++ main.cpp
@@ -4,65 +4,56 @@
 // 2. implement the interface you are working with
 // Without those, nautilus is not going register the extension
 ///////////////////////////////////////////////////////////////
+#include "CreateFileExtension.hpp"
+#include "create_file_extension_logic.hpp"
 #include <cstddef>
-#include <create_file_extension_logic.hpp>
-#include <nautilus-extension.h>
 #include <glib-object.h>
 #include <glib.h>
-#include <gtk/gtk.h> 
+#include <gtk/gtk.h>
+#include <nautilus-extension.h>
 #include <string.h>
-#include <create_file_extension_logic.hpp>
-#include <CreateFileExtension.hpp>
 extern "C" {
-  /////////////////////////////////////////////////////////////
-  // Initialization of Interface you are going to work with
-  /////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+// Initialization of Interface you are going to work with
+/////////////////////////////////////////////////////////////
 
-  // 1. implementing the interface
-  // in this case the backgound context menu (right click on nautilus's backgound)
-  static void menu_provider_iface_init(NautilusMenuProviderInterface *iface) {
-    iface->get_background_items = get_background_items;
-  }
-
-  static GType type_id = 0;
-
-  /////////////////////////////////////////////////////////////
-  // Registration of your extension 
-  /////////////////////////////////////////////////////////////
-
-  void nautilus_module_initialize(GTypeModule *module) {
-
-    type_id = g_type_module_register_type(
-        module, 
-        G_TYPE_OBJECT, 
-        "CreateFileExtension",
-        &*create_file_extension_get_info(),
-        (GTypeFlags)0
-        );
-
-    static const GInterfaceInfo menu_info = {
-      (GInterfaceInitFunc) menu_provider_iface_init, NULL, NULL
-    };
-    g_type_module_add_interface (module, type_id, NAUTILUS_TYPE_MENU_PROVIDER, &menu_info);
-
-    g_print("libnautilus_ext: initialized\n");
-    (void)module;
-  }
-
-  void nautilus_module_list_types(const GType **types, int *num_types) {
-    static GType type_list[1];
-    type_list[0] = type_id;
-
-    *types = type_list;
-    *num_types = 1;
-
-    g_print("libnautilus_ext: types listed\n");
-    g_print("libnautilus_ext: loaded\n");
-  }
-
-  void nautilus_module_shutdown(void) {
-    g_print("libnautilus_ext: shutdown\n");
-  }
-
+// 1. implementing the interface
+// in this case the backgound context menu (right click on nautilus's backgound)
+static void menu_provider_iface_init(NautilusMenuProviderInterface *iface) {
+  iface->get_background_items = get_background_items;
 }
 
+static GType type_id = 0;
+
+/////////////////////////////////////////////////////////////
+// Registration of your extension
+/////////////////////////////////////////////////////////////
+
+void nautilus_module_initialize(GTypeModule *module) {
+
+  type_id = g_type_module_register_type(
+      module, G_TYPE_OBJECT, "CreateFileExtension",
+      &*create_file_extension_get_info(), (GTypeFlags)0);
+
+  static const GInterfaceInfo menu_info = {
+      (GInterfaceInitFunc)menu_provider_iface_init, NULL, NULL};
+  g_type_module_add_interface(module, type_id, NAUTILUS_TYPE_MENU_PROVIDER,
+                              &menu_info);
+
+  g_print("libnautilus_ext: initialized\n");
+  (void)module;
+}
+
+void nautilus_module_list_types(const GType **types, int *num_types) {
+  static GType type_list[1];
+  type_list[0] = type_id;
+
+  *types = type_list;
+  *num_types = 1;
+
+  g_print("libnautilus_ext: types listed\n");
+  g_print("libnautilus_ext: loaded\n");
+}
+
+void nautilus_module_shutdown(void) { g_print("libnautilus_ext: shutdown\n"); }
+}
--- src/create_file_extension_logic.cpp
+++ src/create_file_extension_logic.cpp
@@ -1,114 +1,140 @@
 ////////////////////////////////////////////////////////
 // Extension Logic (what it does)
 ///////////////////////////////////////////////////////
-// Logic can_button
 #include "glib-object.h"
 #include "glib.h"
+#include "glibconfig.h"
 #include "gtk/gtk.h"
 #include "gtk/gtkshortcut.h"
+#include <cstdio>
+#include <filesystem>
+#include <fstream>
+#include <grp.h>
 #include <iostream>
 #include <nautilus-extension.h>
-#include <fstream>
+#include <pwd.h>
+#include <string>
+#include <sys/stat.h>
+#include <unistd.h>
 
-// Logic acc_button
 struct CurrentLocation {
-  GtkWidget* entry;
-  char* path;
+  GtkWidget *entry;
+  char *path;
 };
 
 struct DialogHandler {
-  private:
-    GtkWindow* dialog;
+private:
+  GtkWindow *dialog;
 
-    bool is_set(){
-      if(dialog == nullptr){
-        std::cerr << "Dialog was not set!";
-        return false;
-      }
-      return true;
+  bool is_set() {
+    if (dialog == nullptr) {
+      std::cerr << "Dialog was not set!";
+      return false;
     }
+    return true;
+  }
 
-  public:
-    bool open(){
-      gtk_window_present(dialog);
-      return is_active();
-    };
-    bool close(){
-      gtk_window_destroy(dialog);
-      return is_active();
-    };
-    bool is_active(){
-
-      return gtk_window_is_active(dialog);
-    };
-    void set_dialog(GtkWindow* new_dialog_window){
-      dialog = new_dialog_window;
-    }
+public:
+  bool open() {
+    gtk_window_present(dialog);
+    return is_active();
+  };
+  bool close() {
+    gtk_window_destroy(dialog);
+    return is_active();
+  };
+  bool is_active() { return gtk_window_is_active(dialog); };
+  void set_dialog(GtkWindow *new_dialog_window) { dialog = new_dialog_window; }
 };
 
-DialogHandler* dialog_handler = new DialogHandler();
+DialogHandler *dialog_handler = new DialogHandler();
 
 // BUTTONS Logic
-void can_button_clicked(GtkButton* button, gpointer user_data) {
-  // parameter "button" ignore, is only for jumping over the automatic insersion of GTK
-  GtkWindow* window = GTK_WINDOW(user_data);
+//
+// Logic can_button
+void can_button_clicked(GtkButton *button, gpointer user_data) {
+  // parameter "button" ignore, is only for jumping over the automatic insersion
+  // of GTK
+  GtkWindow *window = GTK_WINDOW(user_data);
   dialog_handler->close();
 }
 
-void acc_button_clicked(GtkButton* button, gpointer user_data) {
+// Logic acc_button
+//
+// for root always uid=0(root) gid=0(root) groups=0(root)
+char *get_file_owner_id(char *path) {
+  g_print("path in : %s", path);
+  struct stat file_stats;
+  stat(path, &file_stats);
+  struct passwd *pw = getpwuid(file_stats.st_uid);
 
-  CurrentLocation* data = static_cast<CurrentLocation*>(user_data);
-  GtkEntryBuffer* buffer = gtk_entry_get_buffer(GTK_ENTRY(data->entry));
-  const char* input_literal = gtk_entry_buffer_get_text(buffer);
+  return g_strdup(pw->pw_name);
+}
 
-  if(sizeof(input_literal) == 0){
+void acc_button_clicked(GtkButton *button, gpointer user_data) {
+
+  CurrentLocation *data = static_cast<CurrentLocation *>(user_data);
+  GtkEntryBuffer *buffer = gtk_entry_get_buffer(GTK_ENTRY(data->entry));
+  const char *input_literal = gtk_entry_buffer_get_text(buffer);
+
+  if (sizeof(input_literal) == 0) {
     delete input_literal;
     return;
   }
 
-  GError* error = NULL;
-  char* path_literal = g_filename_from_uri(data->path, NULL, &error);
-  char* full_path = g_build_filename(path_literal, input_literal, NULL);
+  GError *error = NULL;
+  char *path_literal = g_filename_from_uri(data->path, NULL, &error);
+  char *full_path = g_build_filename(path_literal, input_literal, NULL);
 
-  std::ofstream outfile(full_path);
-  outfile.open(full_path);
+  std::string command = std::string("touch ").append(full_path);
 
-  if(outfile.is_open()){
+  char *user_name = get_file_owner_id(path_literal);
+  g_print("user_name = %s, %s, \n", user_name, full_path);
+
+  if (strcmp(user_name, "root") == 0) {
+    std::string root_command = std::string("pkexec ").append(command);
+    system(root_command.c_str());
+    return;
+  } else {
+    system(command.c_str());
+  }
+
+  if (std::filesystem::exists(full_path)) {
     dialog_handler->close();
-  } 
+  }
 
-  outfile.close();  
+  g_free(user_name);
 }
 
-void acc_button_change_state(GtkButton* button, GtkEntry* entry){
+void acc_button_change_state(GtkButton *button, GtkEntry *entry) {
 
-  GtkEntryBuffer* buffer = gtk_entry_get_buffer(GTK_ENTRY(entry));
-  const char* input_literal = gtk_entry_buffer_get_text(buffer);
+  GtkEntryBuffer *buffer = gtk_entry_get_buffer(GTK_ENTRY(entry));
+  const char *input_literal = gtk_entry_buffer_get_text(buffer);
 
   gtk_widget_set_sensitive(GTK_WIDGET(button), strlen(input_literal) > 0);
   // gtk_widget_add_css_class(GTK_WIDGET(button), "suggested-action");
 }
 
-// FACTORY 
-GtkWindow* dialog_factory(GtkWindow* parent, char* path){
+// FACTORY
+GtkWindow *dialog_factory(GtkWindow *parent, char *path) {
 
   // window
-  GtkWidget* window = gtk_window_new();
+  GtkWidget *window = gtk_window_new();
   gtk_window_set_transient_for(GTK_WINDOW(window), parent);
   gtk_window_set_modal(GTK_WINDOW(window), true);
   gtk_window_set_resizable(GTK_WINDOW(window), false);
   gtk_window_set_title((GTK_WINDOW(window)), "New File");
   gtk_window_set_default_size(GTK_WINDOW(window), 300, 100);
-  
+
   // titlebar
-  GtkWidget* titlebar = gtk_header_bar_new();
+  GtkWidget *titlebar = gtk_header_bar_new();
   gtk_header_bar_set_use_native_controls(GTK_HEADER_BAR(titlebar), true);
 
   // box
-  GtkWidget* box_main = gtk_box_new(GTK_ORIENTATION_VERTICAL, 20);
-  GtkWidget* box_entry = gtk_box_new(GTK_ORIENTATION_VERTICAL, 20);
-  GtkWidget* box_buttons = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 20);
-  
+  GtkWidget *box_main = gtk_box_new(GTK_ORIENTATION_VERTICAL, 20);
+  GtkWidget *box_entry = gtk_box_new(GTK_ORIENTATION_VERTICAL, 20);
+  GtkWidget *box_buttons = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 20);
+
   gtk_box_set_homogeneous(GTK_BOX(box_main), true);
   gtk_widget_set_margin_start(box_main, 15);
   gtk_widget_set_margin_end(box_main, 15);
@@ -119,43 +145,31 @@ GtkWindow* dialog_factory(GtkWindow* parent, char* path){
   gtk_box_set_homogeneous(GTK_BOX(box_buttons), true);
   gtk_box_set_spacing(GTK_BOX(box_main), 20);
 
-  // entry 
-  GtkEntryBuffer* buffer = gtk_entry_buffer_new(NULL, -1);
-  GtkWidget* entry = gtk_entry_new_with_buffer(buffer);
+  // entry
+  GtkEntryBuffer *buffer = gtk_entry_buffer_new(NULL, -1);
+  GtkWidget *entry = gtk_entry_new_with_buffer(buffer);
 
   // buttons
-  GtkWidget* acc_button = gtk_button_new_with_label("Create");
-  GtkWidget* can_button = gtk_button_new_with_label("Cancel");
+  GtkWidget *acc_button = gtk_button_new_with_label("Create");
+  GtkWidget *can_button = gtk_button_new_with_label("Cancel");
 
   acc_button_change_state(GTK_BUTTON(acc_button), GTK_ENTRY(entry));
-  
+
   // BUTTONS WIRERING
-  // Details: 
-  // on_can_button_clicked gets 2 params 
+  // Details:
+  // on_can_button_clicked gets 2 params
   // 1st param button -> represent by first param in g_signal_connect
   // 2nd param window -> represented by the last param in g_signal_connect
-  g_signal_connect(
-      can_button,
-      "clicked",
-      G_CALLBACK(can_button_clicked),
-      window
-      );
+  g_signal_connect(can_button, "clicked", G_CALLBACK(can_button_clicked),
+                   window);
 
-  CurrentLocation* data = new CurrentLocation(entry, path);
+  CurrentLocation *data = new CurrentLocation(entry, path);
 
-  g_signal_connect(
-      acc_button,
-      "clicked",
-      G_CALLBACK(acc_button_clicked),
-      data
-      );
+  g_signal_connect(acc_button, "clicked", G_CALLBACK(acc_button_clicked), data);
 
-  g_signal_connect_swapped(
-      entry,
-      "changed",
-      G_CALLBACK(acc_button_change_state),
-      GTK_BUTTON(acc_button)
-      );
+  g_signal_connect_swapped(entry, "changed",
+                           G_CALLBACK(acc_button_change_state),
+                           GTK_BUTTON(acc_button));
 
   // appending
   gtk_box_append(GTK_BOX(box_entry), entry);
@@ -163,7 +177,7 @@ GtkWindow* dialog_factory(GtkWindow* parent, char* path){
   gtk_box_append(GTK_BOX(box_buttons), acc_button);
   gtk_box_append(GTK_BOX(box_main), box_entry);
   gtk_box_append(GTK_BOX(box_main), box_buttons);
-  
+
   gtk_window_set_child(GTK_WINDOW(window), box_main);
   gtk_window_set_titlebar(GTK_WINDOW(window), titlebar);
 
@@ -173,33 +187,28 @@ GtkWindow* dialog_factory(GtkWindow* parent, char* path){
 // What happens when you press the option
 void on_click(NautilusMenuItem *item, gpointer user_data) {
   // 'item' is the button that was clicked
-  // 'user_data' is whatever pointer you passed during registration (e.g., the folder path)    
-  char* path = (char*) user_data;
-  GtkWindow *parent_window = gtk_application_get_active_window(GTK_APPLICATION(g_application_get_default()));
+  // 'user_data' is whatever pointer you passed during registration (e.g., the
+  // folder path)
+  char *path = (char *)user_data;
+  GtkWindow *parent_window = gtk_application_get_active_window(
+      GTK_APPLICATION(g_application_get_default()));
 
   dialog_handler->set_dialog(dialog_factory(parent_window, path));
   dialog_handler->open();
-} 
+}
 
-extern "C" GList* get_background_items(NautilusMenuProvider *provider, NautilusFileInfo *info) {
+extern "C" GList *get_background_items(NautilusMenuProvider *provider,
+                                       NautilusFileInfo *info) {
 
   // Create your item
   NautilusMenuItem *item = nautilus_menu_item_new(
-      "CreateFileExtension::NewFileAction",
-      "Create File...",
-      "Create a new file",
-      "document-new"
-      );
+      "CreateFileExtension::NewFileAction", "Create File...",
+      "Create a new file", "document-new");
+
   char *uri = nautilus_file_info_get_uri(info);
 
-  // activate item
-  g_signal_connect_data(item, 
-      "activate", 
-      G_CALLBACK(on_click), 
-      uri,             
-      (GClosureNotify)g_free,
-      (GConnectFlags)0);
+  g_signal_connect_data(item, "activate", G_CALLBACK(on_click), uri,
+                        (GClosureNotify)g_free, (GConnectFlags)0);
 
   return g_list_append(NULL, item);
 }
-
